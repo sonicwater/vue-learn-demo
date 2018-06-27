62e6bd1ed8eaa7f8b8fd3ac0cb97a1d7fc5cb36e
sonicwater <sonicwater@126.com>
2018-06-27 23:25:47 +0800

    my-pro

---
+++ my-project/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import App from './App.vue'
+
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+	routes
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
