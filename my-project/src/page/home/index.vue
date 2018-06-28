4b3351e5c2cffa617187863e1b9d9e03b8299ff5
sonic <sonicwater@126.com>
2018-06-28 09:44:19 +0800

    learn

--- my-project/src/page/home/index.vue
+++ my-project/src/page/home/index.vue
@@ -1,7 +1,10 @@
 <template>
   	<div class="home_container">
-        <!-- <itemcontainer father-component="home"></itemcontainer> -->
-        home
+          <!-- <itemcontainer father-component="home"></itemcontainer> -->
+          {{home}}
+          <span v-bind:title="message">
+              鼠标悬停几秒钟查看此处动态绑定的提示信息！
+          </span>
     </div>
 </template>
 
@@ -9,13 +12,19 @@
 // import itemcontainer from '../../components/itemcontainer'
 
 export default {
-	  // name: 'home',
+	  name: 'home',
+    data(){
+        return {
+            home : "sonic",
+            message: '页面加载于 ' + new Date().toLocaleString()
+        }
+    }
   	// components: {
-  	// 	itemcontainer
+  	// 	  itemcontainer
   	// },
-   //  created(){
+    // created(){
         
-   //  }
+    // }
 }
 </script>
 
