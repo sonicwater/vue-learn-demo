3c3a194d55337c7666cb8331abc9c499f678d782
sonic <sonicwater@126.com>
2018-07-03 16:22:47 +0800

    route

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,10 +1,8 @@
 <template>
     <div>
-    	<p>
-			<router-link to="/">home</router-link>
-	    	<router-link to="/item">item</router-link>
-	    	<router-link to="/score">score</router-link>
-    	</p>
+        <router-link to="/">home</router-link>
+        <router-link to="/item">item</router-link>
+        <router-link to="/score">score</router-link>
         <router-view></router-view>
     </div>
 </template>
