21bd74efe7cf6399ff67f60e6a7a37ba15346bb4
sonic <sonicwater@126.com>
2018-08-06 13:56:17 +0800

    mock

---
+++ vue-vuex-mockjs/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in items">{{item.id}}</li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+import Mock from 'mockjs';
+const data = Mock.mock({
+  // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+  'list|1-10': [{
+    // 属性 id 是一个自增数，起始值为 1，每次增 1
+    'id|+1': 1
+  }]
+});
+console.log(data.list)
+export default {
+  name: 'app',
+  data () {
+    return {
+      items: data.list
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
